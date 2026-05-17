d656fa4ad365538cc4a9e864a74a7ff80f3a030e
Niyati Kansal <niyatikansal95@gmail.com>
2026-05-18 01:01:48 +0530

    Backend done

--- Backend/controllers/getTransactions.js
+++ Backend/controllers/getTransactions.js
@@ -2,20 +2,27 @@ import transaction from "../models/transactionHistories.js";
 import STATUS_CODES from "../utils/StatusCodes.js";
 
 const getTransaction=async(req,res)=>{
-    const userid=req.user.id;
+    try{
+        const userid=req.user.id;
 
-    const response=await transaction.find({senderId: userid}).select('senderId recieverId amount status createdAt');
-    if(!response){
-        res.status(STATUS_CODES).json({
-            success: false,
-            msg: "Please try again!"
+        const response = await transaction.find({ $or: [{ senderId: userid }, { receiverId: userid }] }).select('senderId recieverId amount status createdAt');
+        if(!response){
+            res.status(STATUS_CODES).json({
+                success: false,
+                msg: "Please try again!"
+            })
+            return;
+        }
+        res.status(STATUS_CODES.OK).json({
+            success: true,
+            history: response
         })
-        return;
+    }catch(err){
+        res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            msg: "Refresh and try Again!"
+        })
     }
-    res.status(STATUS_CODES.OK).json({
-        success: true,
-        history: response
-    })
 }
 
 export default getTransaction
